e1bf712ef7b9a2a5ff515f68468c216ff4d1a622
indyjones101 <indy.green@workstream.is>
2026-03-06 13:14:09 -0700

    Update find-locations.js

--- api/find-locations.js
+++ api/find-locations.js
@@ -1,6 +1,8 @@
 // api/find-locations.js
 // Given a brand name, finds franchise career page URLs to scan
 
+export const maxDuration = 60;
+
 export default async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method not allowed" });
